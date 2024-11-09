a9fb87e482332c1cd219e93a2c522f5523e9eec7
KaungMinSett <KaungMinSat@my.uopeople.edu>
2024-11-09 16:56:57 +0700

    control views using js

--- network/models.py
+++ network/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "likes": self.likes.count()
+        }
--- network/static/network/styles.js
+++ network/static/network/styles.js
@@ -1,5 +1,13 @@
 
     document.addEventListener('DOMContentLoaded', function() {
+          // Use buttons to toggle between views
+        document.querySelector('#allPosts').addEventListener('click', () => load_post('posts'));
+        document.querySelector('#following').addEventListener('click', () => load_post('following'));
+        document.querySelector('#profile').addEventListener('click', () => load_post('profile'));
+        document.querySelector('#newPost').addEventListener('click', compose_post);
+
+        load_post('posts');
+
         // Get all nav links
         const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 
@@ -13,4 +21,24 @@
                 this.parentElement.classList.add('active');
             });
         });
+
+        
     });
+
+    function load_post(postType) { 
+        document.querySelector('#new-post-view').style.display = 'none';
+        document.querySelector('#post-view').style.display = 'block';
+
+        document.querySelector('#post-view').innerHTML = `<h3>${postType.charAt(0).toUpperCase() + postType.slice(1)}</h3>`;
+
+
+
+    }
+
+    function compose_post() {
+        // Hide all posts
+        document.querySelector('#post-view').style.display = 'none';
+
+        document.querySelector('#new-post-view').style.display = 'block';
+
+    }
